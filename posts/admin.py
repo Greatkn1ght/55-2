3fe05184b8dd0fff84f2221eea5eda4e88c5adb3
Greatkn1ght <greatkn1gh1@gmail.com>
2025-08-12 21:18:04 +0600

    Done 3

--- posts/admin.py
+++ posts/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from posts.models import Post
+from posts.models import Post, Tag, Category
 # Register your models here.
 
 admin.site.register(Post)
+
+admin.site.register(Tag)
+
+admin.site.register(Category)
